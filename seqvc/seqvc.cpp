2ce87967a78005d9c7680d543b07da1823e35637
qPCR4vir <arielvina@yahoo.es>
2022-03-04 17:22:36 +0100

    FIX?

--- seqvc/seqvc.cpp
+++ seqvc/seqvc.cpp
@@ -4,30 +4,35 @@
 #include <string>
 #include <filesystem>
 
+constexpr int desc_LEN = 5000;
+char desc[desc_LEN];
+
 constexpr int LEN = 50000;
-char line[LEN];
+char seq[LEN];
 
-
-inline void putseq(std::ifstream& file_in, std::ofstream& file, int len)
+inline void putseq(std::ifstream& file_in, std::ofstream& file, std::streamsize len)
 {
-    file.write(line, len - 1);
+    file.write(desc, len - 1);  // std::streamsize{ 1 }
     file.put('\n');
-    if (!file_in.getline(line, LEN))
+
+    if (!file_in.getline(seq, LEN))
     {
         std::cerr << "Warning, end of file ! Len=" << len << '\n';
         char a;
-        std::cin >> a;
+        system("pause");
         exit(-1);
     }
-    len = file_in.gcount();
+    std::streamsize s_len = file_in.gcount();
 
-    if (1 < len && len < LEN)
+    if (1 < s_len && s_len < LEN)
     {
-        file.write(line, file_in.gcount() - 1);
+        file.write(seq, s_len - 1);
     }
     else
-        std::cerr << "Warning, bad line ! Len=" << len << '\n';
-
+    {
+        std::cerr << "Warning, bad seq ! Description: '" << std::string(desc, len) << "'. Len = " << len << '\n';
+        std::cerr << "Seq Len=" << s_len << "Seq: " << std::string(seq, s_len) << '\n';
+    }
     file.put('\n');
 }
 
@@ -35,7 +40,7 @@ int main(int argc, char** argv)
 {
     std::filesystem::path in_fasta;
 
-    if (argc == 1 && argv && argv[1])
+    if (argc == 2 && argv && argv[1])
     {
         in_fasta = std::filesystem::path(argv[1]);
     }
@@ -60,41 +65,50 @@ int main(int argc, char** argv)
     long e{ 0L }, n{ 0L }, s{ 0L }, t{ 0L }, m{ (1L << 15) - 1 };
     std::cout << "m= " << m << "\n";
 
-    while (file_in.getline(line, LEN))
+    while (file_in.getline(desc, desc_LEN))
     {
-        int len = file_in.gcount();
+        std::streamsize len = file_in.gcount();
         if (len < 3)
         {
-            std::cerr << "Warning, blank line ! '" << std::string(line, len) << "'. Len = " << len << '\n';
+            std::cerr << "Warning, blank line ! '" << std::string(desc, len) << "'. Len = " << len << '\n';
             continue;
         }
-        if (line[0] != '>')
+        if (desc[0] != '>')
         {
-            std::cerr << "Warning, unknow line ! '" << std::string(line, len) << "'. Len = " << len << '\n';
+            std::cerr << "Warning, unknow line ! '" << std::string(desc, len) << "'. Len = " << len << '\n';
             continue;
         }
 
-        if (line[1] == 'E') 
+        if (desc[1] == 'E') 
         { 
             putseq(file_in, file_E, len);
             ++e; 
         }
-        else if (line[2] == '|' and line[1] == 'N')
+        else if (desc[2] == '|' and desc[1] == 'N')
         { 
             putseq(file_in, file_N, len);
             ++n; 
         }
-        else if (line[1] == 'S')
+        else if (desc[1] == 'S')
         { 
-            putseq(file_in, file_N, len);
+            putseq(file_in, file_S, len);
             ++s; 
+        }
+        else
+        {
+            if (!file_in.getline(seq, LEN))
+            {
+                std::cerr << "Warning, end of file ! Len=" << len << '\n';
+                char a;
+                system("pause");
+                exit(-1);
+            }
         }
         if (!(++t & m)) std::cout << "T= " << t << "N= " << n << ", E= " << e << ", Spike= " << s << "\n";
     }
     std::cout << "T= " << t << ". N= " << n << ", E= " << e << ", Spike= " << s << "\n";
 
-    char a;
-    std::cin >> a;
+    system("pause");
     return 0;
 }
 
