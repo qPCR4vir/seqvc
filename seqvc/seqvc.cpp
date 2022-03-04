d933482a7bb0fd79c7a2046f72e865e9057d2764
qPCR4vir <arielvina@yahoo.es>
2022-03-04 16:17:31 +0100

    check format

--- seqvc/seqvc.cpp
+++ seqvc/seqvc.cpp
@@ -4,56 +4,97 @@
 #include <string>
 #include <filesystem>
 
+constexpr int LEN = 50000;
+char line[LEN];
 
-int main()
+
+inline void putseq(std::ifstream& file_in, std::ofstream& file, int len)
 {
-    std::filesystem::path base_dir{ "D:/PMS/CoV" }; // get the directory
-    std::cout << 1 << base_dir << "\n";
+    file.write(line, len - 1);
+    file.put('\n');
+    if (!file_in.getline(line, LEN))
+    {
+        std::cerr << "Warning, end of file ! Len=" << len << '\n';
+        char a;
+        std::cin >> a;
+        exit(-1);
+    }
+    len = file_in.gcount();
+
+    if (1 < len && len < LEN)
+    {
+        file.write(line, file_in.gcount() - 1);
+    }
+    else
+        std::cerr << "Warning, bad line ! Len=" << len << '\n';
+
+    file.put('\n');
+}
+
+int main(int argc, char** argv)
+{
+    std::filesystem::path in_fasta;
+
+    if (argc == 1 && argv && argv[1])
+    {
+        in_fasta = std::filesystem::path(argv[1]);
+    }
+    else
+    {
+        in_fasta = std::filesystem::path("D:/PMS/CoV/allnuc0209.fasta");
+    }
+    
+
+    std::cout << "Imput fasta file: " << in_fasta << "\n";
+
+    std::filesystem::path base_dir = in_fasta.parent_path();
+    std::string name = in_fasta.stem().string();
+    std::cout << "Imput fasta name: " << name << "\n";
 
     // Initialise a file input object with a FASTA file.
-    std::ifstream file_in{ base_dir / "allnuc0209.fasta" };
-    std::ofstream file_E{ base_dir / "Evc.20220209.fasta" };
-    std::ofstream file_N{ base_dir / "Nvc.20220209.fasta" };
-    std::ofstream file_S{ base_dir / "Svc.20220209.fasta" };
+    std::ifstream file_in{ in_fasta };
+    std::ofstream file_E{ name + ".E-vc.fasta"};
+    std::ofstream file_N{ name + ".N-vc.fasta" };
+    std::ofstream file_S{ name + ".S-vc.fasta" };
 
     long e{ 0L }, n{ 0L }, s{ 0L }, t{ 0L }, m{ (1L << 15) - 1 };
     std::cout << "m= " << m << "\n";
-    constexpr int LEN = 20000;
-    char line[LEN];
 
     while (file_in.getline(line, LEN))
     {
+        int len = file_in.gcount();
+        if (len < 3)
+        {
+            std::cerr << "Warning, blank line ! '" << std::string(line, len) << "'. Len = " << len << '\n';
+            continue;
+        }
+        if (line[0] != '>')
+        {
+            std::cerr << "Warning, unknow line ! '" << std::string(line, len) << "'. Len = " << len << '\n';
+            continue;
+        }
+
         if (line[1] == 'E') 
         { 
-            file_E.write(line, file_in.gcount()-1); 
-            file_E.put('\n');
-            file_in.getline(line, LEN);
-            file_E.write(line, file_in.gcount()-1);
-            file_E.put('\n');
+            putseq(file_in, file_E, len);
             ++e; 
         }
         else if (line[2] == '|' and line[1] == 'N')
         { 
-            file_N.write(line, file_in.gcount()-1);
-            file_N.put('\n');
-            file_in.getline(line, LEN);
-            file_N.write(line, file_in.gcount()-1);
-            file_N.put('\n');
+            putseq(file_in, file_N, len);
             ++n; 
         }
         else if (line[1] == 'S')
         { 
-            file_S.write(line, file_in.gcount()-1);
-            file_S.put('\n');
-            file_in.getline(line, LEN);
-            file_S.write(line, file_in.gcount()-1);
-            file_S.put('\n');
+            putseq(file_in, file_N, len);
             ++s; 
         }
         if (!(++t & m)) std::cout << "T= " << t << "N= " << n << ", E= " << e << ", Spike= " << s << "\n";
     }
     std::cout << "T= " << t << ". N= " << n << ", E= " << e << ", Spike= " << s << "\n";
 
+    char a;
+    std::cin >> a;
     return 0;
 }
 
